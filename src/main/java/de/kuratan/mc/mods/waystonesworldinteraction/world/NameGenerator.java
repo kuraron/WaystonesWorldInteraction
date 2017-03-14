46b97f97ad920fcd564ad9829f5f054c1da8d28c
Kuraron <kuraron.kuratan@gmail.com>
2017-03-14 16:03:17 +0100

    More reference cleanup

---
+++ src/main/java/de/kuratan/mc/mods/waystonesworldinteraction/world/NameGenerator.java
@@ -0,0 +1,31 @@
+package de.kuratan.mc.mods.waystonesworldinteraction.world;
+
+import net.minecraft.world.biome.Biome;
+
+import java.util.Random;
+
+public class NameGenerator {
+    public enum WaystoneLocation {
+        NONE(null),
+        VILLAGE("Village"),
+        STRONGHOLD("Stronghold");
+
+        public final String suffix;
+
+        WaystoneLocation(String suffix) {
+            this.suffix = suffix;
+        }
+    }
+
+    public static String getName(Biome biome, Random random) {
+        return getName(biome, random, WaystoneLocation.NONE);
+    }
+
+    public static String getName(Biome biome, Random random, WaystoneLocation waystoneLocation) {
+        String name = net.blay09.mods.waystones.worldgen.NameGenerator.getName(biome, random);
+        if (waystoneLocation.suffix != null) {
+            return name + " " + waystoneLocation.suffix;
+        }
+        return name;
+    }
+}
